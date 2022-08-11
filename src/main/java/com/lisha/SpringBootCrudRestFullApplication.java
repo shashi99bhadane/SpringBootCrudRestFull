7ec92d65cf77d8238c861a742ffea00c1a3030b9
lishaahire33 <lishaahire33@gmail.com>
2022-08-11 13:20:52 +0530

    project added to central repository

---
+++ src/main/java/com/lisha/SpringBootCrudRestFullApplication.java
@@ -0,0 +1,13 @@
+package com.lisha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudRestFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lisha/SpringBootCrudRestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudRestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
